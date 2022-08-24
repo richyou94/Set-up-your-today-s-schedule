ecd59c5e41f714c592fbc35274a45eb3224ccf8b
Richard You <richardyou94@gmail.com>
2022-08-24 09:57:18 -0700

    added function comparing Time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,151 +1,41 @@
-// setting initial var for current moment and inputing the text to current Date
-var currentTime = moment().format("dddd, MMMM Do");
+// setting initial var for current moment and 
+// inputing the text to current Date
+var currentDate = moment().format("dddd, MMMM Do");
+var currentHour = moment().format("H")
 var todayEl = $('#currentDay');
-todayEl.text(currentTime);
 
+// var testingPast = moment("8AM", "hA").format("H");
+// var testingPresent = moment("9AM", "hA").format("H");
+// var testingFuture = moment("5PM", "hA").format("H");
 
-function saveSchedule () {
-    
+// setting today's date in the text
+todayEl.text(currentDate);
+
+// finding time status
+function compareTime(selectedTime) {
+    if (selectedTime - currentHour < 0) {
+        console.log("past");
+        return "past";
+    } else if (selectedTime - currentHour == 0) {
+        console.log("present")
+        return "present";
+    } else if (selectedTime - currentHour > 0) {
+        console.log("future");
+        return "future";
+    } else {
+        console.log("error");
+    }
 }
 
+var rowEl = $('.row');
+console.log(rowEl.length);
 
-// createRows(9, 17);
-// function createRows(A, B) {
-// for (var i = A; i < B + 1; i++) {
-    // setting var for time table
-// var rowEl = $('<div>');
-// var hourEl = $('<div>');
-// var descriptionEl = $('<textarea>');
-// var btnEl = $('<btn>');
-// var timeBlockEl = $('#timeBlockEl');
-// var btnImageEl = $('<img>');
+// coloring the schedules with grey, red and green depends on 
+// past, present and future
+function renderColor () {
+    for (var i = 0; i < rowEl.length; i++) {
+        console.log(rowEl[i]);
+    }
+}
 
-// var startingHour = moment(i + " AM", "HA");
-// hourEl.text(startingHour.format("HA"));
-// console.log(startingHour.format("HA"));
-// btnImageEl.attr('src', './assets/images/image-save-button.png');
-
-// rowEl.addClass('row');
-// hourEl.addClass('hour');
-// descriptionEl.addClass('description');
-// btnEl.addClass('saveBtn');
-// btnImageEl.addClass('mx-auto d-block');
-
-// btnEl.attr('style', 'padding-top:25px;');
-
-// btnEl.append(btnImageEl);
-
-// rowEl.append(hourEl);
-// rowEl.append(descriptionEl);
-// rowEl.append(btnEl);
-
-// timeBlockEl.append(rowEl);
-// }
-// }
-
-
-// testing the functions
-// var startingHour = moment("9 AM", "H A")
-// console.log(startingHour);
-// var endingHour = moment("17 PM", "H A")
-// console.log(endingHour);
-// startingHour.diff(endingHour, 'hours')
-// endingHour.diff(startingHour, 'hours')
-// console.log(startingHour.diff(endingHour, 'hours'))
-// console.log(endingHour.diff(startingHour, 'hours'))
-
-// function testFunc() {
-//     if (startingHour < endingHour) {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
-// console.log(testFunc());
-
-
-
-// var currentHour = moment().format("HA");
-// console.log(currentHour);
-// var a = moment([2007, 0, 28]);
-// var b = moment([2007, 0, 29]);
-// a.from(b) // "a day ago"
-// console.log(a.from(b))
-
-
-// testingCheckingHour();
-
-// function testingCheckingHour() {
-// var hourTimeEl = moment().format("h A");
-//     console.log(hourTimeEl);
-// }
-
-
-
-
-// Creating new element jQuery tag
-// var titleEl = $('<h1>');
-// ----------------------------
-//Adding text
-// titleEl.text('Hello friends');
-// ----------------------------
-// Adding class
-// titleEl.addClass('p-5');
-// ----------------------------
-// adjusting css
-// titleEl.css('border', 'rgb(122, 242, 242) 5px solid');
-// ----------------------------
-// appending new element
-// rootEl.append(titleEl);
-// ----------------------------
-// addEventLIstener
-// alertButtonEl.on('click', function () {
-//   alert('Hello World');
-// });
-// ----------------------------
-
-// function handleFormSubmit(event) {
-//     // Prevent the default behavior
-//     event.preventDefault();
-  
-//     console.log('First Name:', firstNameEl.val());
-//     console.log('Last Name:', lastNameEl.val());
-//     console.log('Email:', emailEl.val());
-//     console.log('GitHub:', githubEl.val());
-  
-//     // Select all checked options
-//     var checkedEl = $('input:checked');
-//     var selected = [];
-  
-//     // Loop through checked options to store in array
-//     $.each(checkedEl, function () {
-//       selected.push($(this).val());
-//     });
-//     console.log('Toppings: ', selected.join(', '));
-  
-//     // Clear input fields
-//     $('input[type="text"]').val('');
-//     $('input[type="email"]').val('');
-//     $('input[type="checkbox"]').prop('checked', false);
-//   }
-  
-//   // Submit event on the form
-//   formEl.on('submit', handleFormSubmit);
-// ----------------------------
-
-// rootEl.children('ul').children().css('background-color', 'white');
-
-// Select the last `<ul>` or the third row.
-// Long traverals can be segmented to provide better readability.
-// var rowThree = rootEl.children('ul').eq(2);
-
-// Select the children of the `<ul>`, then select the first box and add the 'O'.
-// rowThree.children().eq(0).text('O');
-// ----------------------------
-
-// clearEl.on('click', function () {
-//     displayEl.empty();
-//   });
-// ----------------------------
-// var btnClicked = $(event.target);
-// ----------------------------
+renderColor();
